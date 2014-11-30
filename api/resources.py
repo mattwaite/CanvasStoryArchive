a5c7e33a0a001f38789d58857bf1f471aaeb0fab
Liam Andrew <landrew@mit.edu>
2014-11-30 07:48:38 +0300

    Set up view that processes fulltext

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,30 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
-from stories.models import Story
+from stories.models import Story, Entity, EntityType
+
+class EntityTypeResource(ModelResource):
+
+    class Meta:
+        queryset = EntityType.objects.all()
+        resource_name = 'entity_type'
+        allowed_methods = ['get']
+
+
+class EntityResource(ModelResource):
+    entity_type = fields.ForeignKey(EntityTypeResource, 'entity_type', full=True)
+    #story_set = fields.ManyToManyField('api.resources.StoryResource', 'story_set', full=True)
+
+    class Meta:
+        queryset = Entity.objects.all()
+        resource_name = 'entity'
+        allowed_methods = ['get']
+
+    def determine_format(self, request):
+        return 'application/json'
 
 class StoryResource(ModelResource):
-    
+    entities = fields.ManyToManyField(EntityResource, 'entities', full=True)
+
     class Meta:
         queryset = Story.objects.all()
         resource_name = 'story'
