ad1a6b719219af53d355e960b128dc6b32de4169
Matt Waite <matt.waite@gmail.com>
2014-11-30 01:34:37 -0600

    Adding a couple of bits to story admin, including search and display

--- stories/admin.py
+++ stories/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from stories.models import EntityType, Entity, Story, NounCount, RelatedStories
 
+
+class StoryAdmin(admin.ModelAdmin):
+    search_fields = ['headline']
+    list_display = ('headline', 'pubdate')
+
 admin.site.register(EntityType)
 admin.site.register(Entity)
-admin.site.register(Story)
+admin.site.register(Story, StoryAdmin)
 admin.site.register(NounCount)
 admin.site.register(RelatedStories)
